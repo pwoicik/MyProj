e7f957c551217c256ce6a2a1fe40a35db53fa95a
pwoicik <patrykwoicik@gmail.com>
2023-02-28 21:55:41 +0100

    Initial Commit

---
+++ MyProj/MyProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProjApp.swift
+//  MyProj
+//
+//  Created by Patryk Wójcik on 28/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
